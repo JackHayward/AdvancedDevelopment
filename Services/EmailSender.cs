cd31483e1f8429703b985cba265c08b05b5dab3e
Jack Hayward <imjackhayward@gmail.com>
2020-01-05 01:12:03 +0000

    Cleaning up code

--- Services/EmailSender.cs
+++ Services/EmailSender.cs
@@ -12,18 +12,15 @@ namespace AdvancedDevelopment.Services
 {
     public class EmailSender : IEmailSender
     {
-        private readonly IConfiguration _configuration;
-        private readonly String SendGridUser;
-        private readonly String SendGridKey;
+        private readonly String _sendGridUser;
+        private readonly String _sendGridKey;
 
         public EmailSender(IConfiguration configuration)
         {
-            _configuration = configuration;
+            KeyGrabber keyGrabber = new KeyGrabber(configuration);
 
-            KeyGrabber keyGrabber = new KeyGrabber(_configuration);
-
-            SendGridKey = keyGrabber.GetKey("SendGridKey");
-            SendGridUser = keyGrabber.GetKey("SendGridUser");
+            _sendGridKey = keyGrabber.GetKey("SendGridKey");
+            _sendGridUser = keyGrabber.GetKey("SendGridUser");
         }
 
         //public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
@@ -33,7 +30,7 @@ namespace AdvancedDevelopment.Services
 
         public Task SendEmailAsync(string email, string subject, string message)
         {
-            return Execute(SendGridKey, subject, message, email);
+            return Execute(_sendGridKey, subject, message, email);
         }
 
         public Task Execute(string apiKey, string subject, string message, string email)
